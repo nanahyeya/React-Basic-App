44585c0b4ef670edac05575414a95220883aa314
nahye <knh8783@gmail.com>
2025-05-23 00:28:02 +0900

    add probs mycomponent

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+  render() {
+    return (
+        <div>
+            Hello ! {this.props.name}
+      ``</div>
+    )
+  }
+}
