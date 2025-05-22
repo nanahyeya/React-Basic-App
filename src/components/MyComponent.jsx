28ae63f78239bde0aa4426390a6d9a8c53d4e97d
nahye <knh8783@gmail.com>
2025-05-23 00:43:32 +0900

    add proptypes

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
-
+import { Component } from "react";
+import PropTypes from 'prop-types';
 export default class MyComponent extends Component {
   render() {
     return (
         <div>
-            Hello ! {this.props.name}
+            <h3>Hello ! {this.props.name}</h3>
       ``</div>
     )
   }
-}
+} // class
+
+MyComponent.defaultProps = {
+    name: '리액트JS'
+}; //defaultProps
+
+MyComponent.propTypes = {
+    name: PropTypes.string //name props 타입을 문자열로 설정함
+};
